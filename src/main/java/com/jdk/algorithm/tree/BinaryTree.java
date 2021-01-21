d010f1dbc39f6af8eb1ac3274bcd34c321986cb8
jm005807 <bing.zhang@baozun.com>
2021-01-21 16:47:16 +0800

    二叉树遍历

---
+++ src/main/java/com/jdk/algorithm/tree/BinaryTree.java
@@ -0,0 +1,91 @@
+package com.jdk.algorithm.tree;
+
+import java.util.*;
+
+/**
+ * @description: 二叉树
+ * @author: zhangbing
+ * @create: 2021-01-21 14:14
+ **/
+public class BinaryTree {
+
+    public static void main(String[] args) {
+
+        Node node = creact();
+
+        System.out.println("=========前序=========");
+        preOrder(node);
+
+        System.out.println("=========中序=========");
+        middleOrder(node);
+
+        System.out.println("=========后序=========");
+
+        System.out.println("==========每层从左向右遍历==============");
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+
+        while (queue.size() != 0) {
+
+            Node n = queue.poll();
+            System.out.println(n.val);
+
+            if (n.left != null) {
+                queue.add(n.left);
+            }
+            if (n.right != null) {
+                queue.add(n.right);
+            }
+        }
+
+
+    }
+
+    private static void middleOrder(Node node) {
+        if (node==null){
+            return;
+        }
+
+        middleOrder(node.left);
+        System.out.println(node.val);
+        middleOrder(node.right);
+    }
+
+    public static void preOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.println(root.val);
+        preOrder(root.left);
+        preOrder(root.right);
+
+
+    }
+
+
+    public static Node creact() {
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        Node f = new Node(6);
+        Node i = new Node(7);
+        Node j = new Node(8);
+        Node k = new Node(9);
+        Node l = new Node(10);
+        Node g = new Node(11);
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        c.left = f;
+        c.right = i;
+        d.left = j;
+        d.right = k;
+        e.right = l;
+        e.left = g;
+        return a;
+    }
+}
---
+++ src/main/java/com/jdk/algorithm/tree/Node.java
@@ -0,0 +1,18 @@
+package com.jdk.algorithm.tree;
+
+/**
+ * @description:
+ * @author: zhangbing
+ * @create: 2021-01-21 14:14
+ **/
+public class Node {
+
+    int val;
+    Node right = null;
+    Node left = null;
+
+    Node(int val) {
+        this.val = val;
+    }
+
+}
