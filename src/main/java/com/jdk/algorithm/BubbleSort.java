9f080509a727e97ec1ca71dc96cfc7a99fd639d0
jm005807 <bing.zhang@baozun.com>
2021-01-21 17:42:03 +0800

    二叉树遍历

--- src/main/java/com/jdk/algorithm/BubbleSort.java
+++ src/main/java/com/jdk/algorithm/BubbleSort.java
@@ -18,32 +18,32 @@ public class BubbleSort {
         /**
          * 正序
          */
-//        int i = array.length - 1;
-//        while (i >= 0) {
-//            for (int j = 0; j < i; j++) {
-//                if (array[j] > array[j + 1]) {
-//                    int tmp = array[j + 1];
-//                    array[j + 1] = array[j];
-//                    array[j] = tmp;
-//                }
-//            }
-//            i--;
-//        }
+        int i = array.length - 1;
+        while (i >= 0) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int tmp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = tmp;
+                }
+            }
+            i--;
+        }
 
         /**
          * 倒序
          */
-        int i = 0;
-        while (i < array.length) {
-            for (int j = array.length - 1; j > i; j--) {
-                if (array[j] > array[j - 1]) {
-                    int tmp = array[j - 1];
-                    array[j - 1] = array[j];
-                    array[j] = tmp;
-                }
-            }
-            i++;
-        }
+//        int i = 0;
+//        while (i < array.length) {
+//            for (int j = array.length - 1; j > i; j--) {
+//                if (array[j] > array[j - 1]) {
+//                    int tmp = array[j - 1];
+//                    array[j - 1] = array[j];
+//                    array[j] = tmp;
+//                }
+//            }
+//            i++;
+//        }
 
         for (int k : array) {
             System.out.println(k);
